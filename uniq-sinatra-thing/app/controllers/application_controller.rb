206cc7fdb63f6afc7d49670b15f53af32c0cec10
Dj <ritchey0713@gmail.com>
2019-05-31 20:59:55 -0400

    corneal project

---
+++ uniq-sinatra-thing/app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  #CRUD routes
+
+  get "/" do
+    erb :welcome
+  end
+
+end
